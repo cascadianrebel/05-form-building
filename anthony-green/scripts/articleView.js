00b44261221a27c00a6b678f166b4f6f52336ed4
Anthony Green <Anthonydgreen90@gmail.com>
2018-04-29 20:28:53 -0700

    corrected previous mistakes

--- anthony-green/scripts/articleView.js
+++ anthony-green/scripts/articleView.js
@@ -75,37 +75,46 @@ articleView.setTeasers = () => {
 };
 
 // COMMENT: Where is this function called? Why?
-// This is called after the other function calls. 
+// This is called in new.html so it would execute at page load
 articleView.initNewArticlePage = () => {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-  $('tab-content').show();
 
-  // TODO: The new articles we create will be copy/pasted into our source data file.
-  // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
+  $('.tab-content').show();
 
   $('#article-json').on('focus', function(){
-    this.select();
+    $(this).select();
+    document.execCommand('copy');
   });
 
   // TODO: Add an event handler to update the preview and the export field if any inputs change.
-
+  $('#new-article').on('change','input, textarea', articleView.create);
 };
 
 articleView.create = () => {
   // TODO: Set up a variable to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
+  $('#articles *').remove();
 
 
   // TODO: Instantiate an article based on what's in the form fields:
-
+  let newInput = {
+    title: $('#title').val(),
+    category: $('#category').val(),
+    author: $('#author').val(),
+    authorUrl: $('#authorUrl').val(),
+    publishedOn: $('#publishedOn').val(),
+    body: $('#body').val(),
+  }
+  let article = new Article(newInput);
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
 
+  $('#articles').append(article.toHtml());
 
   // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each():
   $('pre code').each();
 
   // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#article-json').val(JSON.stringify(article));
 
 };
 
